496d4aa4bfa2d82ddb0f921d2f439b42d9bc672e
Jihwan Kim <eric967712345@gmail.com>
2019-09-01 22:19:48 +0900

    Add url

--- DailyMaily_API/urls.py
+++ DailyMaily_API/urls.py
@@ -1,4 +1,5 @@
-"""DailyMaily_API URL Configuration
+"""
+DailyMaily_API URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
